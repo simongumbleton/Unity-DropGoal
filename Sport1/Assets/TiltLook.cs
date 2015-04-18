e71c82080bc67eed2f06c36013dd4a041387dc76
simongumbleton <simongumbleton@gmail.com>
2015-04-18 18:11:28 +0100

    adding iPhone input before trying new package

---
+++ Sport1/Assets/TiltLook.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltLook : MonoBehaviour {
+	
+	private Gyroscope gyro;
+	
+	void Start ()
+
+	{
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		if (SystemInfo.supportsGyroscope) {
+			gyro = Input.gyro;
+			gyro.enabled = true;
+		}
+		//Input.compensateSensors = false;
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		//if ((Input.deviceOrientation == DeviceOrientation.LandscapeLeft) || (Input.deviceOrientation == DeviceOrientation.LandscapeRight))
+		transform.localRotation = Input.gyro.attitude;
+
+	}
+}
