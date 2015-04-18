bb2c85c641d90f47e7e2f8e9402eafc29262184c
simongumbleton <simongumbleton@gmail.com>
2015-04-18 20:25:53 +0100

    Gyro camera using device orientation so landscape works properly

--- Sport1/Assets/Scripts/iPhoneGyroCam.cs
+++ Sport1/Assets/Scripts/iPhoneGyroCam.cs
@@ -11,34 +11,57 @@ public class iPhoneGyroCam : MonoBehaviour {
 	private bool gyroBool;
 	private Gyroscope gyro;
 	private Quaternion rotFix;
+	private Quaternion lastCamRot;
+	public float smoothRate;
+	GameObject camParent;
+	DeviceOrientation orientation;
 	
 	void Start() {
 		
 		Transform originalParent = transform.parent; // check if this transform has a parent
-		GameObject camParent = new GameObject ("camParent"); // make a new parent
+		camParent = new GameObject ("camParent"); // make a new parent
 		camParent.transform.position = transform.position; // move the new parent to this transform position
-		transform.parent = camParent.transform; // make this transform a child of the new parent
-		camParent.transform.parent = originalParent; // make the new parent a child of the original parent
+		//transform.parent = camParent.transform; // make this transform a child of the new parent
+		transform.SetParent (camParent.transform);
+		//camParent.transform.parent = originalParent; // make the new parent a child of the original parent
+		camParent.transform.SetParent (originalParent);
+		lastCamRot = transform.localRotation;
 		
 		gyroBool = Input.isGyroAvailable;
+		;
 		
 		if (gyroBool) {
-			
+			orientation = Input.deviceOrientation;
+			print (orientation);
 			gyro = Input.gyro;
 			gyro.enabled = true;
 			
-			if (Screen.orientation == ScreenOrientation.LandscapeLeft) {
-				camParent.transform.eulerAngles = new Vector3(90,90,0);
-			} else if (Screen.orientation == ScreenOrientation.Portrait) {
+//			if ((Screen.orientation == ScreenOrientation.LandscapeLeft)||(Screen.orientation == ScreenOrientation.LandscapeRight)) {
+//				camParent.transform.eulerAngles = new Vector3(90,90,0);
+//			} else if (Screen.orientation == ScreenOrientation.Portrait) {
+//				camParent.transform.eulerAngles = new Vector3(90,180,0);
+//			}
+//			
+//			if ((Screen.orientation == ScreenOrientation.LandscapeLeft)||(Screen.orientation == ScreenOrientation.LandscapeRight)) {
+//				rotFix = new Quaternion(0f,0f,0.7071f,0.7071f);
+//			} else if (Screen.orientation == ScreenOrientation.Portrait) {
+//				rotFix = new Quaternion(0f,0f,1f,0f);
+//			}
+			//Screen.sleepTimeout = 0;
+
+			if ((orientation == DeviceOrientation.LandscapeLeft)||(orientation == DeviceOrientation.LandscapeRight)) {
+				camParent.transform.eulerAngles = new Vector3(90,90,180);
+			} else if (orientation == DeviceOrientation.Portrait) {
 				camParent.transform.eulerAngles = new Vector3(90,180,0);
 			}
 			
-			if (Screen.orientation == ScreenOrientation.LandscapeLeft) {
-				rotFix = new Quaternion(0f,0f,0.7071f,0.7071f);
-			} else if (Screen.orientation == ScreenOrientation.Portrait) {
+			if ((orientation == DeviceOrientation.LandscapeLeft)||(orientation == DeviceOrientation.LandscapeRight)) {
+				rotFix = new Quaternion(0f,0f,1f,0f);
+			} else if (orientation == DeviceOrientation.Portrait) {
 				rotFix = new Quaternion(0f,0f,1f,0f);
 			}
-			//Screen.sleepTimeout = 0;
+
+
 		} else {
 			print("NO GYRO");
 		}
@@ -46,9 +69,24 @@ public class iPhoneGyroCam : MonoBehaviour {
 	
 	void Update () {
 		if (gyroBool) {
+
+			//orientation = Input.deviceOrientation;
+			//print (orientation);
+
+
 			Quaternion camRot = gyro.attitude * rotFix;
-			transform.localRotation = camRot;
+			//camRot = camRot * smoothRate;
+			//transform.localRotation = camRot;
+
+
+			transform.localRotation = Quaternion.Slerp(lastCamRot,camRot,smoothRate*Time.deltaTime);
+
+			lastCamRot = camRot;
 		}
+	}
+
+	void OnDestroy(){
+		orientation = Input.deviceOrientation;
 	}
 
 }
